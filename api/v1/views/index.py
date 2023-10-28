673236321e437b05f51a4156a59ccc0d4b5f4e60
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-28 05:08:33 -0700

    Strict slashes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """Functio to execute status route logic"""
     return jsonify({"status": "OK"})
