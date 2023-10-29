6d38e51f78a18139bdd96be6c1fe1c76fe50183b
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-29 05:50:35 -0700

    Status endpoint fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 import models
 from models import storage
 from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.amenity import Amenity
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -17,9 +23,15 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats_count():
     """'/stats' route that retrieves and display json Responses"""
-    todos = {'states': State, 'users': User,
-             'amenities': Amenity, 'cities': City,
-             'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
+    classes = {
+            "amenities": Amenity,
+            "cities": City,
+            "places": Place,
+            "reviews": Review,
+            "states": State,
+            "users": User
+            }
+    todos = {}
+    for key, val in classes.items():
+        todos[key] = storage.count(val)
     return jsonify(todos)
