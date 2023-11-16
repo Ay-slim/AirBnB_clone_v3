4651dba058eb182323670819e347a5ebed4b337e
Damicable <annobidiegwu@gmail.com>
2023-11-16 00:42:27 +0000

    Corrected

--- api/v1/app.py
+++ api/v1/app.py
@@ -33,10 +33,10 @@ def error(e):
 #    'uiversion': 3
 #}
 
-Swagger(app)
+#Swagger(app)
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
-    app.run(host=host, port=port, threaded=True)
+    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', default=5000)
+    app.run(host, int(port), debug=True)
