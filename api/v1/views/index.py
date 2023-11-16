fef57d15ae767905682be4a8a7212eb63236b61e
Damicable <annobidiegwu@gmail.com>
2023-11-16 03:11:46 +0000

    Done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,15 +3,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-import models
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import user
-
 
 @app_views.route("/status", strict_slashes=False)
 def view_status():
