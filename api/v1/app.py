b9db77a4d029c34d1d9bab8a8876389c001a278a
Damicable <annobidiegwu@gmail.com>
2023-11-15 18:48:26 -0800

    Added

--- api/v1/app.py
+++ api/v1/app.py
@@ -37,6 +37,6 @@ def error(e):
 
 
 if __name__ == "__main__":
-     host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
+    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
     port = os.getenv('HBNB_API_PORT', default=5000)
     app.run(host, int(port), debug=True)
