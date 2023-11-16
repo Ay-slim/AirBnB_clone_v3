312f0a2fe1296ea44c9641e0ebb57ee2f1ff86f0
Damicable <annobidiegwu@gmail.com>
2023-11-15 19:00:16 -0800

    Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -39,4 +39,4 @@ def error(e):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
     port = os.getenv('HBNB_API_PORT', default=5000)
-    app.run(host, int(port), debug=True)
+    app.run(host=host, port=port, threaded=True)
