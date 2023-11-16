8cd54dca569fe8a5a1164a04afc0b9c6a229ee34
Damicable <annobidiegwu@gmail.com>
2023-11-15 18:26:18 -0800

    commited

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""Module contains routes status"""
+"""Flask application that handles status route declaration"""
+
 from api.v1.views import app_views
 from flask import jsonify
+import models
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import user
 
 
 @app_views.route("/status", strict_slashes=False)
-def status():
-    """
-    Returns a JSON status.
-    """
+def view_status():
+    """ View function that return a json message."""
     return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats", strict_slashes=False)
-def stats():
-    """
-    Retrieves the number of each objects by type
-    """
+def view_stats():
+    """ Veiw function that retrieves the number of each object by type."""
     return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
     })
